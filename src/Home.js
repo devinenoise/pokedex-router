442396c1eddfdb1a93afa71098dec3fb7840bffa
Kyle Devine <devinenoise@gmail.com>
2020-02-17 11:24:31 -0800

    url searching working

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     state = {
-        searchQuery: this.props.match.params.pokemon,
+        searchQuery: this.props.match.params.name,
         characters: [],
     }
     
     async componentDidMount() {
-        if (this.props.match.params.pokemon) {
-            const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.props.match.params.pokemon}`)
+        if (this.props.match.params.name) {
+            const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.props.match.params.name}`)
 
             this.setState({ characters: data.body.results })
         }
